c56e158d938801cd1e178b1f6cf404ead01916fd
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-05 09:22:55 +0100

    First commit jour04 > job02

---
+++ jour04/job02.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3.12
+# -*- coding: utf-8 -*-
+
+class Personne:
+    
+    def __init__(self, age = 14):
+        self.age = age
+
+    def afficherAge(self):
+        print(f"Âge de la personne : {self.age} an(s)")
+
+    def bonjour(self):
+        print("Hello")
+
+    def modifierAge(self):
+        nouvel_age = int(input("Entrez le nouvel âge de la personne : "))
+        self.age = nouvel_age
+
+
+class Eleve(Personne):
+    
+    def allerEnCours(self):
+        print("Je vais en cours")
+
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans")
+
+
+class Professeur(Personne):
+    
+    def __init__(self, matiereEnseignee, age=14):
+        super().__init__(age)
+        self.matiereEnseignee = matiereEnseignee
+
+    def enseigner(self):
+        print("Le cours va commencer")
+
+
+# Instanciation d'une personne et d'un élève
+personne1 = Personne()
+eleve1 = Eleve()
+
+# Affichage de l'âge par défaut de l'élève
+eleve1.afficherAge()
+
+# Test de la méthode modifierAge pour la personne
+personne1.afficherAge()
+personne1.bonjour()
+personne1.modifierAge()
+
+# Affichage de l'âge mis à jour
+personne1.afficherAge()
+
+# Instanciation d'un professeur
+professeur1 = Professeur(matiereEnseignee="Informatique", age=40)
+
+# Appel des méthodes spécifiées pour l'élève et le professeur
+eleve1.bonjour()
+eleve1.allerEnCours()
+eleve1.modifierAge(15)
+eleve1.afficherAge()
+
+professeur1.bonjour()
+professeur1.enseigner()
